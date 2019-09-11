72f6755f31d92952467c6f9df9c3fad9281582f7
nepta1998 <36112164+nepta1998@users.noreply.github.com>
2019-09-11 10:35:00 -0400

    agregando la tabla servicio.

---
+++ src/main/java/com/proyecto/laboratorio/controller/Servicio.java
@@ -0,0 +1,12 @@
+package com.proyecto.laboratorio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Servicio {
+    @GetMapping({"/tableService"})
+    public String index() {
+        return"tableService";
+    }
+}
