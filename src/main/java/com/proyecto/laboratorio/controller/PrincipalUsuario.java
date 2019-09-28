0ffb3eacce0810a3c55705322554fc44f081825f
joclpacheb <joclpacheb@hotmail.com>
2019-09-28 00:04:32 -0400

    Cosas editadas: Se cambiaron todas las referencias a /index por "/indexgob" y se actualizó el controlador PrincipalUsuario.
    Se agregaron nuevos docs html para las tablas reporte de cada fundacion (vistas del usuario gobernación), su control está a cargo de principalusuario.
    
    Se colocó en el footer "Laboratorio I UCLA DCYT"
    
    Se agregó en indexgob los 3 botones.

--- src/main/java/com/proyecto/laboratorio/controller/PrincipalUsuario.java
+++ src/main/java/com/proyecto/laboratorio/controller/PrincipalUsuario.java
@@ -6,8 +6,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PrincipalUsuario {
 
-    @GetMapping({"/index"})
+    @GetMapping({"/indexgob"})
     public String index() {
-        return"index";
+        return"indexgob";
     }
+
+    @GetMapping({"/tableFunDisc"})
+    public String tableFunDisc() {
+        return"tableFunDisc";
+    }
+
+    @GetMapping({"/tableFunMujer"})
+    public String tableFunMujer() {
+        return"tableFunMujer";
+    }
+
+    @GetMapping({"/tableFunNinnos"})
+    public String tableFunNinnos() {
+        return"tableFunNinnos";
+    }
+
 }
