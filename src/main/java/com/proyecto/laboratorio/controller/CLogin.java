6ab85693443700998e53fa3b55353f3c8b27bbc8
dany <danykaram97@gmail.com>
2019-11-17 02:01:14 -0400

    Spring Security Part I. This only the beginning.

--- src/main/java/com/proyecto/laboratorio/controller/CLogin.java
+++ src/main/java/com/proyecto/laboratorio/controller/CLogin.java
@@ -1,13 +1,24 @@
 package com.proyecto.laboratorio.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CLogin {
 
-    @GetMapping({"/","/login"})
+   /* @GetMapping({"/","/login"})
     public String login() {
+        return "login";
+    }*/
+
+    /*@GetMapping({"/","/login"})
+    public String mostrarLogin(Model model)
+    {
+        return "login";
+    }*/
+    @GetMapping({"/","/login"})
+    public String index() {
         return "login";
     }
 }
--- src/main/java/com/proyecto/laboratorio/controller/CPrincipalUsuario.java
+++ src/main/java/com/proyecto/laboratorio/controller/CPrincipalUsuario.java
@@ -1,20 +1,21 @@
 package com.proyecto.laboratorio.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CPrincipalUsuario {
 
-   /* @GetMapping({"/index"})
-    public String index() {
-        return"index";
-    }*/
-
     @GetMapping({"/index"})
     public String index() {
-        return "index";
+        return"index";
     }
+
+   /* @GetMapping({"/index"})
+    public String showIndex(Model model) {
+        return "index";
+    }*/
 
    /* @GetMapping({"/tableFunDisc"})
     public String tableFunDisc() {
